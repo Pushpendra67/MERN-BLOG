470c3ff91fbb974cf3224bd83e2a9035cab184b0
Pushpendra Singh <hexatanwar@gmail.com>
2024-05-29 15:32:29 +0530

    signup api and ui designing

--- api/index.js
+++ api/index.js
@@ -3,9 +3,18 @@ const app=express();
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 dotenv.config();
-import router from "./routes/user.route.js";
+import userRoute from "./routes/user.route.js";
+// import router from "./routes/user.route.js";
+ import authRoute from "./routes/auth.route.js";
+// app.use("/api",router);
 
-app.use("/api",router);
+app.use(express.json())
+// app.use(express.text())  
+app.use(express.urlencoded({extended:false}))
+
+app.use("/api",authRoute);
+app.use("/api",userRoute);
+
 
 
 mongoose.connect(process.env.MONGO).then((result) => {
@@ -22,3 +31,16 @@ mongoose.connect(process.env.MONGO).then((result) => {
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
 })
+
+
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,9 @@
 import express from "express"
 const router=express.Router()
 import test from "../controller/user.controller.js"
-
+// import signup from "../controller/auth.controller.js"
 
 router.get("/test",test)
-
+// router.post('/signup', signup)
 
 export default router;
